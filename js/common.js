d4025d19c70138c211a33e47ee53c0d4aa01e4e2
Vladislav <vladd0193@gmail.com>
2019-03-14 11:46:06 +0200

    gallery end

--- js/common.js
+++ js/common.js
@@ -1,11 +1,34 @@
 document.addEventListener("DOMContentLoaded", initJs)
 
-
-
 function initJs() {
 	const d = document;
-	const w = window;
-	console.log('document loaded');
+    const w = window;
+    const gallery = d.querySelector('.gallery');
+    
+
+
+    if(gallery) {
+        const filter   = [...d.querySelectorAll('.gallery__filter li')];
+        const articles = [...d.querySelectorAll('.gallery__article')];
+        filter.forEach(el => {
+            el.addEventListener('click', e => {
+                e.preventDefault();
+                filter.forEach(el => el.classList.remove('active'));
+                el.classList.add('active');
+                const data = el.dataset.filter;
+                if(data =='all') {
+                    articles.forEach(el => el.style.display = 'block');
+                } else {
+                    const targ = d.querySelectorAll('.gallery__article[data-filter='+data+']');
+                    articles.forEach(el => el.style.display = 'none');
+                    targ.forEach(el => el.style.display = 'block');
+                };
+            });
+        });
+    }
+
+
+
 }
 
 function autoHeight(el) {
