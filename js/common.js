71fe087d4a6880ea384fe842f5a46cfc4db4cd47
Vladislav <vladd0193@gmail.com>
2019-03-19 16:47:11 +0200

    in progress

--- js/common.js
+++ js/common.js
@@ -4,7 +4,9 @@ function initJs() {
 	const d = document;
     const w = window;
     const gallery = d.querySelector('.gallery');
-    
+    const sslider = d.querySelector('.comp3--slider .comp3__items');
+    const pslider = d.querySelector('.portfolio__list');
+    const vslider = d.querySelectorAll('.comp6__items');
 
 
     if(gallery) {
@@ -27,6 +29,31 @@ function initJs() {
         });
     }
 
+    const slickObj = {
+        service: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true
+        },
+        portfolio: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: true
+        },
+        blog: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true 
+        }
+    }
+
+
+    $(sslider).slick(slickObj.service);
+    $(pslider).slick(slickObj.portfolio);
+    vslider.forEach(sl => $(sl).slick(slickObj.blog));
+
+
+
 
 
 }
@@ -34,3 +61,24 @@ function initJs() {
 function autoHeight(el) {
 	el.style.height = (el.scrollHeight) + "px";
 }
+
+function initMap() {
+    const gMap = {
+      Keletska: {
+        position: {lat: 49.224684, lng: 28.416439},
+        map() { return newMap('contacts-map', this.position) }
+      }
+    };
+    for(let key in gMap) {
+        gMarker(gMap[key].position, gMap[key].map() );
+    }
+    function gMarker(position, map) {
+      return new google.maps.Marker({
+          position,
+          map
+      })
+    }
+    function newMap(id, position) {
+      return new google.maps.Map(document.getElementById(id), {zoom: 16, center: position})
+    }
+  }
